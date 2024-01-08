a8a439aa1f53a37f78568f683778c50e311cd680
Loewenherz12 <146241315+Loewenherz12@users.noreply.github.com>
2024-01-08 22:21:22 +0100

    testenlol

---
+++ Assets/Testen.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //lol wie geht es uns lol hahahhahahah
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
